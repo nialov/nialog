0f41378cc35bd4c3cfcf193156b7f74b6de8d25c
nialov <nikolasovaskainen@hotmail.com>
2021-11-11 22:48:59 +0200

    refactor(nialog): add important imports to init

--- nialog/__init__.py
+++ nialog/__init__.py
@@ -5,3 +5,9 @@ JSON logging setup for Python.
 """
 
 __version__ = "0.0.1"
+
+from nialog.logger import LoggingLevel, setup_module_logging
+
+assert callable(setup_module_logging)
+
+assert isinstance(LoggingLevel.WARNING.value, str)
